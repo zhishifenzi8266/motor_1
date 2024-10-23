a77997bd62a2622d3078ebf19ab0aac78f3c6f71
知识份子 <zhishifenzi8266@gmail.com>
2024-10-24 06:28:06 +0800

    bug fix* OnTriggerEnter & OnCollisionEnter

--- Assets/Scenes/script/player_move.cs
+++ Assets/Scenes/script/player_move.cs
@@ -13,19 +13,13 @@ public class player_move : MonoBehaviour
     private int d = 0;
     public int force = 3;
     public Vector2 vectormove;
-    //public Regidbody body;
-    //public float jump;
-    //public Vector3 newPosotion;
     private Rigidbody rb;
-    public float jump = 0.5f; //跳跃高度
-    public bool isdimian; //检测地面碰撞是否可以跳跃
-    public Collision pengzhuan; //碰撞检测2
+    public float jump = 0.5f; // 跳跃高度
+    public bool isdimian; // 检测地面碰撞是否可以跳跃
     public int bitcoin;
-    public TMPro.TextMeshProUGUI scoretext; //显示分数
-    public TMPro.TextMeshProUGUI warning; //终点提示
-    public float moveSpeed; //摄像头跟踪鼠标
-
-    //private Rigidbody rb:
+    public TMPro.TextMeshProUGUI scoretext; // 显示分数
+    public TMPro.TextMeshProUGUI warning; // 终点提示
+    public float moveSpeed; // 摄像头跟踪鼠标
 
     // Start is called before the first frame update
     void Start()
@@ -34,16 +28,13 @@ public class player_move : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         Debug.Log("游戏开始");
         isdimian = true;
-        moveSpeed = 600f; //摄像头跟踪鼠标移动速度
+        moveSpeed = 600f; // 摄像头跟踪鼠标移动速度
     }
 
     // Update is called once per frame
     void Update()
     {
         vectormove.x = Input.GetAxis("Horizontal");
-        //vectormove.y = Input.GetAxis("Vertival");
-        //Debug.Log("我不好");
-        //测试testing
 
         if (Input.GetKeyDown(KeyCode.T))
         {
@@ -51,85 +42,48 @@ public class player_move : MonoBehaviour
             Debug.Log($"输出T{d}次");
         }
 
-        //人物移动2.0《
-        rb = this.GetComponent<Rigidbody>();
-
         float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
 
-        float h = Input.GetAxis("Horizontal");//得到键盘左右控制
+        rb.AddForce(new Vector3(h, 0, v) * force);
 
-        rb.AddForce(new Vector3(h, 0, v) * force);//对物体施加力
-
-
-        //》
-        //人物移动 《
-        //if (Input.GetKey(KeyCode.W))
-        //{
-        //    transform.Translate(0.0f, 0f, speed);
-        //}
-        //if (Input.GetKey(KeyCode.A))
-        //{
-        //    transform.Translate(-speed, 0f, 0f);
-        //}
-        //if (Input.GetKey(KeyCode.S))
-        //{
-        //    transform.Translate(0f, 0f, -speed);
-        //}
-        //if (Input.GetKey(KeyCode.D))
-        //{
-        //    transform.Translate(speed, 0f, 0f);
-        //}
-        //人物移动 》
-
-        //if(Input.GetKey(KeyCode.Space)) {
-        //    Rigidbody.AddForce(0.0f, speed, 0.0f);
-
-        //(检测是否是地面我的方法(碰撞检测1))Physics=物体 raycast=玩家下面生成一条线检测他的vector往下是否是1,5f 如果不是跳跃不了
-        //isdimian = Physics.Raycast(transform.position, Vector3.down, 1.5f);
-
-
-        //把isdimian函数绑定给jump
         if (Input.GetKeyDown(KeyCode.Space) && isdimian)
         {
             rb.AddForce(0f, jump, 0f, ForceMode.Impulse);
             isdimian = false;
         }
-        //摄像头跟踪鼠标的方法《
-        Vector3 angle = Vector3.zero;
 
+        Vector3 angle = Vector3.zero;
         angle.x = transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * moveSpeed * Time.deltaTime;
         angle.y = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * moveSpeed * Time.deltaTime;
-
         transform.localEulerAngles = angle;
-        //摄像头跟踪鼠标的方法》
-
     }
 
-    //碰撞检测2（老师的方法）
     private void OnCollisionEnter(Collision pengzhuan)
     {
-        Debug.Log("我创到了：" +  pengzhuan.gameObject.tag + "!");
+        Debug.Log("我创到了：" + pengzhuan.gameObject.tag + "!");
 
         if (pengzhuan.gameObject.CompareTag("dimian"))
         {
             isdimian = true;
         }
-        if (pengzhuan.gameObject.CompareTag("KillZone"))//死亡的时候加载地图LoadScene(0)
+        if (pengzhuan.gameObject.CompareTag("KillZone"))
         {
             Debug.Log("KILL ME!!!");
             SceneManager.LoadScene(0);
         }
-        //if (pengzhuan.gameObject.CompareTag("win")) //赢的时候加载地图LoadScene(1)
-        //{
-        //    SceneManager.LoadScene(2);
-        //}
-        if (pengzhuan.gameObject.CompareTag("bitcoin"))
+    }
+
+    void OnTriggerEnter(Collider pengzhuan2)
+    {
+        Debug.Log("我创到了：" + pengzhuan2.gameObject.tag + "!");
+        if (pengzhuan2.gameObject.CompareTag("bitcoin"))
         {
-            Destroy(pengzhuan.gameObject);
+            Destroy(pengzhuan2.gameObject);
             bitcoin++;
             scoretext.text = bitcoin.ToString();
         }
-        if(pengzhuan.gameObject.CompareTag("win"))
+        if (pengzhuan2.gameObject.CompareTag("win"))
         {
             warning.enabled = true;
         }
